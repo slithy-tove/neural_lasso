81d92c22a61220d6c94ba274dad685ae79913621
Elias Leventhal <elias.leventhal@yale.edu>
2026-09-18 17:38:48 -0400

    Added results to gitignore

--- scripts/fit_models.py
+++ scripts/fit_models.py
@@ -4,13 +4,13 @@ import numpy as np
 kwargs = {"model_name" : ["neural_lasso"],
           "dataset_name" : ["boston"],
           "sparsity" : [3],
-          "n_epochs" : [1000],
+          "n_epochs" : [1],
           "batch_size" : [128],
           "lambda_min" : [0],
           "lambda_max" : [1],
           "n_lambda" : [20],
           "reg_type" : ["l1", "l2"],
-          "lr" : [1e-3],
+          "lr" : [1e-4],
           "hdim" : [128],
           "hnum" : [1]}
 

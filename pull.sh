d94115728dd5cea6f90c329c78248a26039d1a57
Elias Leventhal <elias.leventhal@yale.edu>
2026-09-19 10:52:43 -0400

    Updated pull file

--- pull.sh
+++ pull.sh
@@ -1,2 +1,2 @@
 rsync -av -e "ssh -S ~/.ssh/control-%r@%h:%p" \
-  el862@bouchet.ycrc.yale.edu:/home/el862/project_pi_sk2433/el862/SNAB/results/ ./results/
+  el862@bouchet.ycrc.yale.edu:/home/el862/project_pi_sk2433/el862/neural_lasso/results/* ./results

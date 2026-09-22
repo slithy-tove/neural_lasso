987c40ca6f5c7428cfd22ece5f14c5c7f8433544
Elias Leventhal <elias.leventhal@yale.edu>
2026-09-22 15:47:59 -0400

    Added p_reg capability

--- job.sh
+++ job.sh
@@ -3,7 +3,7 @@
 #SBATCH --partition=devel
 #SBATCH --cpus-per-task=4
 #SBATCH --mem-per-cpu=5G
-#SBATCH --time=01:00:00
+#SBATCH --time=02:00:00
 #SBATCH --output=output.txt
 
 module load uv 
--- scripts/fit_models.py
+++ scripts/fit_models.py
@@ -4,13 +4,14 @@ import numpy as np
 kwargs = {"model_name" : ["neural_lasso"],
           "dataset_name" : ["boston"],
           "sparsity" : [3],
-          "n_epochs" : [1000],
+          "n_epochs" : [10000],
           "batch_size" : [None],
           "lambda_min" : [0],
           "lambda_max" : [1],
           "n_lambda" : [20],
-          "reg_type" : ["new1, new2"],
-          "lr" : [1e-4],
+          "reg_type" : ["p_reg"],
+          "reg_p" : [0, 0.25, 0.5, 1, 1.5, 2],
+          "lr" : [1e-3],
           "hdim" : [128],
           "hnum" : [3]}
 

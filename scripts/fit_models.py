d8d3f48201db4c530b3d31fc0732ea432079a3b4
Elias Leventhal <elias.leventhal@yale.edu>
2026-09-24 17:09:52 -0400

    Set up for LassoNet v NeuralLasso run

--- scripts/fit_models.py
+++ scripts/fit_models.py
@@ -2,14 +2,14 @@ from .utils import fit_models
 import numpy as np
 from torch.optim import Adam
 
-kwargs = {"model_name" : ["neural_lasso"],
+kwargs = {"model_name" : ["lasso_net", "neural_lasso"],
           "dataset_name" : ["housing"],
           "sparsity" : [3],
           "batch_size" : [None],
           "lambda_min" : [0],
           "lambda_max" : [1],
           "n_lambda" : [10],
-          "reg_type" : ["l1", "group", "group_squared"],
+          "reg_type" : ["group"],
           "init_lr" : [1e-3],
           "path_lr" : [1e-3],
           "init_epochs" : [10000], 
